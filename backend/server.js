6ba477c2c371a9e445fcf26514536cd568571f0c
SreekanthKumarReddySR <sreekanth29kumar05reddy@gmail.com>
2025-11-09 13:11:06 +0530

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,32 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './app.js';
 import express from 'express';
+import cors from 'cors';
+
 dotenv.config();
 mongoose.set('strictQuery', false);
 
 const PORT = parseInt(process.env.PORT);
 const DB_URI = process.env.MONGO_URI;
+
+//Allow both local and deployed frontends
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://sr-shopping.vercel.app", // your Vercel frontend
+];
+
+// Setup CORS properly
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like Postman or mobile)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) return callback(null, true);
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongoose.connect(DB_URI)
@@ -19,3 +40,4 @@ mongoose.connect(DB_URI)
   .catch((err) => {
     console.log("Error connecting to database:", err);
   });
+
